32790c97a82ea3ab20aa355d1a034d4b05a1851f
Tim <beolol9539@gmail.com>
2018-04-22 13:05:01 +0300

    Create project NewsNow.

---
+++ NewsNow/Scene/News/NewsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NewsViewController.swift
+//  NewsNow
+//
+//  Created by Tim on 22/04/2018.
+//  Copyright © 2018 timothey.taran. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ NewsNow/Scene/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsNow
+//
+//  Created by Tim on 22/04/2018.
+//  Copyright © 2018 timothey.taran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
